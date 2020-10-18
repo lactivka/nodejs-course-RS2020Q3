c81ad6ecd96ce6725fd0888449cab405a096e8af
unknown <lactivo4ka@gmail.com>
2020-10-19 02:04:29 +0300

    feat: Add express middleware which will log incoming requests to service (url, query parameters, body)

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const morgan = require('morgan');
+const winston = require('./common/winston');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
@@ -10,6 +12,25 @@ const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
+
+app.use(
+  morgan(
+    (tokens, req, res) => {
+      console.log(req.params);
+      return [
+        tokens.method(req, res),
+        tokens.status(req, res),
+        tokens.url(req, res),
+        'query:',
+        JSON.stringify(req.params),
+        'body:',
+        JSON.stringify(req.body)
+      ].join(' ');
+    },
+
+    { stream: winston.stream }
+  )
+);
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
